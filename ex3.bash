faa24ec1caf357175ac56b156b03337b254c45a7
shahram moradpour <51191070+strumer69@users.noreply.github.com>
2025-07-28 14:30:02 +0200

    password hiding

---
+++ ex3.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+# USE  -p for one line coding (input from user and the message to the user" 
+read -s -p  "please enter apassword"  password
+echo  #create a new empty line
+echo "pass received from Shiraz:) "
