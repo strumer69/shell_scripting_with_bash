099338792ee3a5179cd216843d417e7451ddb559
shahram <shmoradpour69@gmail.com>
2025-07-29 09:52:38 +0200

    call arguments in bash

---
+++ ex4.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "script name : $0"
+echo "first argument :  $1"
+echo "second argument : $2"
+echo "3th argument : $3"
+echo "number of arguments : $#"
+echo "all arguments (as one string): $*"
+echo "all arguments (as seperate items): $@"
+echo "THD Cafe at K-building is so cozy"
